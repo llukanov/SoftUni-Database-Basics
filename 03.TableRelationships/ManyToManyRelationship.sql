961a39f0e407b7f46e12ff6587685215ca72c6e4
llukanov <60892930+llukanov@users.noreply.github.com>
2020-06-13 17:54:56 +0300

    Create ManyToManyRelationship.sql

---
+++ 03.TableRelationships/ManyToManyRelationship.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Students (
+	[StudentID] INT IDENTITY NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams (
+	[ExamID] INT NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE StudentsExams (
+	[StudentID] INT NOT NULL FOREIGN KEY REFERENCES Students([StudentID]),
+	[ExamID] INT NOT NULL FOREIGN KEY REFERENCES Exams([ExamID])
+	PRIMARY KEY ([StudentID], [ExamID])
+)
+
+INSERT INTO Students ([Name])
+VALUES 
+	('Mila'),
+	('Toni'),
+	('Ron')
+
+INSERT INTO Exams ([ExamID], [Name])
+VALUES
+	(101, 'SpringMVC'),
+	(102, 'Neo4j'),
+	(103, 'Oracle 11g')
+
+INSERT INTO StudentsExams
+VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103)
