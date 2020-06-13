4c6b696f86ed435b49058b4b593e82c02f7d5c71
llukanov <60892930+llukanov@users.noreply.github.com>
2020-06-13 18:59:10 +0300

    Create PeaksInRila.sql

---
+++ 03.TableRelationships/PeaksInRila.sql
@@ -0,0 +1,6 @@
+USE [Geography]
+
+SELECT Mountains.MountainRange, Peaks.PeakName, Peaks.Elevation FROM Mountains
+JOIN Peaks ON Peaks.MountainId = Mountains.Id
+WHERE MountainRange = 'Rila'
+ORDER BY Elevation DESC
